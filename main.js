cbacc55f098c39583de4aa1cd3e8118f4447a113
RGrockstar <74416609+RGrockstar@users.noreply.github.com>
2021-06-08 23:25:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
- 
+status="";
+function setup(){
+    canvas= createCanvas(300, 300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function start(){
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Object Detecting";
+    document.getElementById("name").value;
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status=true;
+}
+function draw(){
+    image(video, 0,0, 480, 380);
+}
